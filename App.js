f552e30a957d2693c6357800bcc07be3947fa799
Mobina Noori <91049843+mobinanoorii@users.noreply.github.com>
2023-07-17 00:28:50 +0330

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+import Favorites from './components/Favorites';
+import Meals from './components/Meals';
+import Modal from './components/Modal';
+import Search from './components/Search';
+import { useGlobalContext } from './context';
+import FavoriteIndicator from './components/FavoriteIndicator';
+
+
+function App() {
+
+  const { showModal, favorites, showFavorite } = useGlobalContext();
+  
+
+  return (
+    <main>
+      <header>
+        <FavoriteIndicator />
+        <Search />
+        {(showFavorite && favorites.length >= 1)&& <Favorites />}
+      </header>
+      
+      <Meals />
+      { showModal && <Modal /> }
+        
+    </main>
+  );
+}
+
+export default App;
